d3e52641f6193f3192b9cb31ef4aa65eeaadd1ea
ash01825 <niteshtulshyan998@gmail.com>
2024-10-29 03:30:28 +0530

    commit

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
+from models import MenuItem
+# app = Flask(__name__)
 
-app = Flask(__name__)
+# @app.route('/', methods= ['GET', 'POST'])
+# def login_page():
+#     return render_template("login-2.html")
 
-@app.route('/', methods= ['GET', 'POST'])
-def login_page():
-    return render_template("login-2.html")
+# @app.route('/login/register')
+# def register_page():
+#     otp=OTPmanager()
+#     return render_template()
+# if __name__ == "__main__":
+#     app.run(debug=True)
+from models import MenuItem
 
-@app.route('/login/register')
-def register_page():
-    otp=OTPmanager()
-    return render_template()
-if __name__ == "__main__":
-    app.run(debug=True)
+# Fetch all items to verify
+menu_items = MenuItem.get_all()
+for item in menu_items:
+    print(f"ID: {item.id}, Name: {item.name}, Price: {item.price}")
